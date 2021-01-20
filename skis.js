242e899e091e1ab870e1d6429ac1d805f77cae02
Alexanderson10 <alexanderson1010@me.com>
2021-01-19 18:05:50 -0800

    product data

---
+++ skis.js
@@ -0,0 +1,55 @@
+export const skis = [
+    {
+        id: 1,
+        image: 'https://s24131.pcdn.co/wp-content/uploads/2019/09/Mindbender108.png',
+        skiLength: '179 cm',
+        skiWidth: '108 mm',
+        skiFlex: 'stiff',
+        preferredCondition: 'all mountain'
+    },
+    {
+        id: 2,
+        image: 'https://summitsports.scene7.com/is/image/SummitSports/313720_313720_1?$600$',
+        skiLength: '189 cm',
+        skiWidth: '120 mm',
+        skiFlex: 'soft',
+        preferredCondition: 'powder',
+    },
+    {
+        id: 3,
+        image: 'https://s22867.pcdn.co/wp-content/uploads/2017/08/K2_Marksman.png',
+        skiLength: '184 cm',
+        skiWidth: '106 mm',
+        skiFlex: 'medium',
+        preferredCondition: 'all mountain',
+    },
+    {
+        id: 4,
+        image: 'https://summitsports.scene7.com/is/image/SummitSports/273755_273755_1?$600$',
+        skiLength: '179 cm',
+        skiWidth: '105 mm',
+        skiFlex: 'soft',
+        preferredCondition: 'all mountain'
+    },
+    {
+        id: 5,
+        image: 'https://summitsports.scene7.com/is/image/SummitSports/564586_564586_1?$600$',
+        skiLength: '184 cm',
+        skiWidth: '82 mm',
+        skiFlex: 'stiff',
+        preferredCondition: 'Groomed',
+    },
+    {
+        id: 6,
+        image: 'https://s22867.pcdn.co/wp-content/uploads/2013/09/K2-Shredditor112-ski.jpg',
+        skiLength: '179 cm',
+        skiWidth: '112 mm',
+        skiFlex: 'soft',
+        preferredCondition: 'powder',
+
+    }
+
+
+
+
+]
